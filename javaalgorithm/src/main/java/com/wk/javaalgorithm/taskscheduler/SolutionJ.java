b400daaa81bb42ddd44c5df408021f7eac4657b7
WuKaiXia <w15110799554@163.com>
2020-12-05 19:26:14 +0800

    任务调度器-基于桶排序,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/taskscheduler/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/taskscheduler/SolutionJ.java
@@ -1,5 +1,6 @@
 package com.wk.javaalgorithm.taskscheduler;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -31,5 +32,20 @@ public class SolutionJ {
         return Math.max(((maxExec - 1) * (n + 1) + maxCount.getAndIncrement()), tasks.length);
     }
 
-
+    public int leastInterval1(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+        Arrays.sort(freq);
+        int maxExec = freq[25];
+        int maxCount = 1;
+        for (int i = 24; i >= 0; i--) {
+            if (freq[i] != maxExec) {
+                break;
+            }
+            maxCount++;
+        }
+        return Math.max((maxExec - 1) * (n + 1) + maxCount, tasks.length);
+    }
 }
