fa4f21fb3672f0ddc89eee74f19abae9f3e65438
WuKaiXia <w15110799554@163.com>
2020-12-07 22:25:31 +0800

    861. 翻转矩阵后的得分- 优化除第一列以外的每列求和逻辑,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/matrixreverse/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/matrixreverse/SolutionJ.java
@@ -9,32 +9,30 @@ public class SolutionJ {
 
     public int matrixScore(int[][] A) {
         if (A == null || A.length < 1) return 0;
-        int n = A.length, m = A[0].length;
-        int result = (int) (Math.pow(2, m - 1) * n);
+        int row = A.length, column = A[0].length;
+        int result = (1 << (column - 1)) * row;
         // 保证最高位都为1
         for (int[] a : A) {
             if (a[0] == 0) {
-                for (int i = 0; i < a.length; i++) {
+                for (int i = 0; i < column; i++) {
                     // 异或运算，相对应位的值相同则为0，否则为1
                     a[i] = a[i] ^ 1;
                 }
             }
         }
-        for (int i = 1; i < m; i++) {
-            int count = 0;
+        // 优化
+        for (int i = 1; i < column; i++) {
+            int k = 0;
+            int reverseNum = 0;
             for (int[] ints : A) {
-                if (ints[i] == 1) count++;
-            }
-
-            if (count < (n + 1) / 2) {
-                for (int[] ints : A) {
-                    ints[i] = ints[i] ^ 1;
+                if (ints[i] == 1) {
+                    k++;
+                } else {
+                    reverseNum++;
                 }
             }
             // 计算每列的和
-            for (int[] ints : A) {
-                result += ints[i] * Math.pow(2, m - 1 - i);
-            }
+            result += Math.max(k, reverseNum) * (1 << (column - 1 - i));
         }
 
         return result;
