8247b32f5b34801174d0d0b43b96a9b6752365e4
WuKaiXia <w15110799554@163.com>
2020-12-15 21:58:04 +0800

    leetcode 738. 单调递增的数字-优化,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/monotoneincreasing/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/monotoneincreasing/SolutionJ.java
@@ -17,8 +17,30 @@ public class SolutionJ {
             if (strN[i] > strN[i + 1]) {
                 strN[i]--;
                 for (int j = i + 1; j < length; j++) {
+                    if (strN[j] == '9') {
+                        break;
+                    }
                     strN[j] = '9';
                 }
+            }
+        }
+        return Integer.parseInt(new String(strN));
+    }
+
+    public static int monotoneIncreasingDigits1(int N) {
+        char[] strN = Integer.toString(N).toCharArray();
+        int length = strN.length;
+        int i = 1;
+        // 高位<低一位的数
+        while (i < length && strN[i - 1] <= strN[i]) {
+            i++;
+        }
+        if (i < length) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[--i]--;
+            }
+            for (i += 1; i < length; i++) {
+                strN[i] = '9';
             }
         }
         return Integer.parseInt(new String(strN));
