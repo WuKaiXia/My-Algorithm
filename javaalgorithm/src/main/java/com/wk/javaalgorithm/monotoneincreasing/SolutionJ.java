d727656479582194d899fca0257a5c5a2aa5fecf
WuKaiXia <w15110799554@163.com>
2020-12-15 21:23:46 +0800

    leetcode 738. 单调递增的数字,Java

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/monotoneincreasing/SolutionJ.java
@@ -0,0 +1,32 @@
+package com.wk.javaalgorithm.monotoneincreasing;
+
+/**
+ * 738. 单调递增的数字
+ */
+public class SolutionJ {
+
+    public static void main(String[] args) {
+        monotoneIncreasingDigits(10);
+    }
+
+    public static int monotoneIncreasingDigits(int N) {
+        if (N < 10) return N;
+        char[] strN = Integer.toString(N).toCharArray();
+        int length = strN.length;
+        for (int i = length - 2; i > -1; i--) {
+            if (strN[i] > strN[i + 1]) {
+                strN[i]--;
+                for (int j = i + 1; j < length; j++) {
+                    strN[j] = '9';
+                }
+            }
+        }
+        String str;
+        if (strN[0] == 0) {
+            str = String.valueOf(strN, 1,length);
+        } else {
+            str = new String(strN);
+        }
+        return Integer.parseInt(str);
+    }
+}
