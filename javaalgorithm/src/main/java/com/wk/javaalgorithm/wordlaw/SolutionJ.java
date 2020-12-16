f2162514bb0e20079440e3ad6ef802191f0edeb4
WuKaiXia <w15110799554@163.com>
2020-12-16 21:20:03 +0800

    leetcode 290. 单词规律-双map,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/wordlaw/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/wordlaw/SolutionJ.java
@@ -29,21 +29,14 @@ public class SolutionJ {
     }
 
     public boolean wordPattern1(String pattern, String s) {
+        String[] tokens = s.split(" ");
+        int length = tokens.length;
+        if (length != pattern.length()) return false;
         HashMap<Character, String> char2Str = new HashMap<>();
         HashMap<String, Character> str2Char = new HashMap<>();
-        int i = 0, strLength = s.length();
-        for (int p = 0; p < pattern.length(); p++) {
-            // 防止p的长度大于s以空格分开的数组长度
-            if (i >= strLength) {
-                return false;
-            }
-            char ch = pattern.charAt(p);
-            // 字符串startIndex
-            int j = i;
-            while (j < strLength && s.charAt(j) != ' ') {
-                j++;
-            }
-            String str = s.substring(i, j);
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String str = tokens[i];
             String temp = char2Str.getOrDefault(ch, null);
             Character tempCh = str2Char.getOrDefault(str, null);
             if ((temp != null && !str.equals(temp)) || (tempCh != null && tempCh != ch)) {
@@ -51,9 +44,7 @@ public class SolutionJ {
             }
             char2Str.put(ch, str);
             str2Char.put(str, ch);
-            // 下轮的字符串startIndex
-            i = j + 1;
         }
-        return i >= strLength;
+        return true;
     }
 }
