1f5ad723021aa8c1ecfdcf2dbd2d048a52f7e4f7
WuKaiXia <w15110799554@163.com>
2020-12-17 21:56:14 +0800

    leetcode 714. 买卖股票的最佳时机含手续费-优化空间复杂度,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/stock/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/stock/SolutionJ.java
@@ -23,17 +23,15 @@ public class SolutionJ {
 
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        // 0: 当天手里没有股票时的收益；1：当天手里有股票时的收益
+        // sell: 当天手里没有股票时的收益；buy：当天手里有股票时的收益
         int[][] dp = new int[n][2];
-        dp[0][0] = 0;
-        // 买股票
-        dp[0][1] = -prices[0];
+        int sell = 0, buy = -prices[0];
         for (int i = 1; i < n; i++) {
             // 前一天没有股票时的收益与前一天有股票时的收益+今天卖出-fee的和相比较的最大值为当天手里没股票时的最大收益
-            dp[i][0] = Math.max(prices[i] + dp[i - 1][1] - fee, dp[i - 1][0]);
+            sell = Math.max(prices[i] + buy - fee, sell);
             // 前一天有股票的收益与前一天没有股票收益-当天购买股票价格相比较
-            dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+            buy = Math.max(sell - prices[i], buy);
         }
-        return dp[n - 1][0];
+        return sell;
     }
 }
