d9bb7e1d2234feb5040e8017b224c4837682722f
WuKaiXia <w15110799554@163.com>
2020-12-21 21:41:17 +0800

    leetcode 746. 使用最小花费爬楼梯-添加注释,Java

--- javaalgorithm/src/main/java/com/wk/javaalgorithm/mincost/SolutionJ.java
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/mincost/SolutionJ.java
@@ -2,9 +2,15 @@ package com.wk.javaalgorithm.mincost;
 
 /**
  * 746. 使用最小花费爬楼梯
+ *
+ * 一次走一个或者两个台阶
  */
 public class SolutionJ {
 
+    /**
+     * d[i] = min(d[i-2]+cost[i-2], d[i-1]+cost[i-1])
+     * 动态规划
+     */
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int prev = 0, curr = 0;
