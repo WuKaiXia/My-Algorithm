8df441ee7a0c0a16dbf11f6c88a0b18775ea05a8
WuKaiXia <w15110799554@163.com>
2021-03-22 21:10:59 +0800

    leetcode 191. 位1的个数

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/leetcode191/SolutionJ.java
@@ -0,0 +1,16 @@
+package com.wk.javaalgorithm.leetcode191;
+
+/**
+ * 191. 位1的个数
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ */
+public class SolutionJ {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            res++;
+        }
+        return res;
+    }
+}
