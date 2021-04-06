c062166ea2625f9c524baf229fb10bb13eecff98
WuKaiXia <w15110799554@163.com>
2021-04-06 21:51:45 +0800

    leetcode 80. 删除有序数组中的重复项 II

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/leetcode80/SolutionJ.java
@@ -0,0 +1,26 @@
+package com.wk.javaalgorithm.leetcode80;
+
+/**
+ * 80. 删除有序数组中的重复项 II
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
+public class SolutionJ {
+
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if (length <= 2) {
+            return length;
+        }
+        int slow = 2, fast = 2;
+        while (fast < length) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+}
