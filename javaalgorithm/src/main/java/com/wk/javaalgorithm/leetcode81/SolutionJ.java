cf0ec88db6857757427abbcf31e32abc3647dbe4
WuKaiXia <w15110799554@163.com>
2021-04-07 21:33:04 +0800

    leetcode 81. 搜索旋转排序数组 II

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/leetcode81/SolutionJ.java
@@ -0,0 +1,44 @@
+package com.wk.javaalgorithm.leetcode81;
+
+/**
+ * 81. 搜索旋转排序数组 II
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ */
+public class SolutionJ {
+
+    public boolean search(int[] nums, int target) {
+        if (nums == null) return false;
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (r - l + 1) / 2 + l;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] == nums[l] && nums[mid] == nums[r]) {
+                l++;
+                r--;
+            } else if (nums[l] <= nums[mid]) {
+                if (target < nums[mid] && nums[l] <= target) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
