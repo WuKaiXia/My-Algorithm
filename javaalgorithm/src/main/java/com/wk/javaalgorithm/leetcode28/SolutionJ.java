77b63a5ebe371964a154011684a437105717b22f
WuKaiXia <w15110799554@163.com>
2021-04-20 21:14:28 +0800

    leetcode 28. 实现 strStr()

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/leetcode28/SolutionJ.java
@@ -0,0 +1,27 @@
+package com.wk.javaalgorithm.leetcode28;
+
+/**
+ * 28. 实现 strStr()
+ * 实现 strStr() 函数。
+ * <p>
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1
+ */
+public class SolutionJ {
+
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i <= n - m; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
