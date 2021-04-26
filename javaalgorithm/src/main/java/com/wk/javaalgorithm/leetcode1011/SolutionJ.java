afea1832141b0c765231629b734f3a912d16f32c
WuKaiXia <w15110799554@163.com>
2021-04-26 21:48:02 +0800

    leetcode 1011. 在 D 天内送达包裹的能力

---
+++ javaalgorithm/src/main/java/com/wk/javaalgorithm/leetcode1011/SolutionJ.java
@@ -0,0 +1,34 @@
+package com.wk.javaalgorithm.leetcode1011;
+
+import java.util.Arrays;
+
+/**
+ * 1011. 在 D 天内送达包裹的能力
+ * 传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+ * <p>
+ * 传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+ * <p>
+ * 返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+ */
+public class SolutionJ {
+    public int shipWithinDays(int[] weights, int D) {
+        int left = Arrays.stream(weights).max().getAsInt(), right = Arrays.stream(weights).sum();
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            int curr = 0, need = 1;
+            for (int weight : weights) {
+                if (curr + weight > mid) {
+                    ++need;
+                    curr = 0;
+                }
+                curr += weight;
+            }
+            if (need <= D) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
